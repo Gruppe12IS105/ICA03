41d74d4da7a92578cc6afc1fba09ddbd56339bc1
VasuraInDrag <indrag16@student.uia.no>
2017-03-02 14:29:26 +0100

    Server som printer ut tekst og tid

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,10 +21,12 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	// Neste linje kan brukes for å endre koding av sekvensen som sendes til nettleser
 	// Standardinstilling er UTF-8
 	//w.Header().Set("Content-Type", "text/html;charset=ISO-8859-1")
-
+	t := time.Now().Format(time.RFC850)
 	// Her skriver man data som er respons til brukeren som har skrevet
 	// http://localhost:3000 i sin nettleser
-	w.Write([]byte("<font color=\"green\">Hvordan g\xe5r det, <b>\u16a6</b> ?</font><br/>"))
+	w.Write([]byte("<font color=\"green\">Hvordan g\u00e5r det, <b>\u16a6</b> ?</font><br/>"))
 	w.Write([]byte("\u16a6 - Thurs<br/>"))
+	w.Write([]byte("\xe2\x8f\xb0"))
+	w.Write([]byte(t))
 
 }
