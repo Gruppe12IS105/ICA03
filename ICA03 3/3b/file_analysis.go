f1098a44642451daf5c119c1a13405a33faa2fcf
Konflux <marign16@student.uia.no>
2017-05-18 23:47:32 +0200

    Ryddet opp i kode

--- ICA03 3/3b/file_analysis.go
+++ ICA03 3/3b/file_analysis.go
@@ -8,8 +8,6 @@ import (
 
 func main() {
 	filename := "./files/lang01.wl"
-	//  filename := "./files/lang02.wl"
-	//  filename := "./files/lang03.wl"
 
 	var f = fileutils.FileToByteslice(filename)
 	fmt.Printf("% X", f)
